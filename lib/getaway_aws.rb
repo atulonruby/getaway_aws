ed7863e48f656500fa3934d32a0cf0ba90c3f14c
atulonruby <atuljha86@gmail.com>
2014-04-03 11:03:06 -0700

    tp

--- lib/getaway_aws.rb
+++ lib/getaway_aws.rb
@@ -1,7 +1,12 @@
-require "getaway_aws/version"
+#require "getaway_aws/version"
+#require 'aws-sdk'
 
 module GetawayAws
   def car
     "first time i saw you, you were chasing down...A Cylcone, all alone in a field"
+  end
+  
+  def get
+    AWS::S3.new
   end
 end
