c277666344a85e5e627888f1add1cf9ca43d9603
atulonruby <atuljha86@gmail.com>
2014-04-03 10:37:16 -0700

    first getaway commit

---
+++ lib/getaway_aws.rb
@@ -0,0 +1,7 @@
+require "getaway_aws/version"
+
+module GetawayAws
+  def car
+    "first time i saw you, you were chasing down...A Cylcone, all alone in a field"
+  end
+end
---
+++ lib/getaway_aws/version.rb
@@ -0,0 +1,3 @@
+module GetawayAws
+  VERSION = "0.0.1"
+end
