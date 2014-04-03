bba27ed159a91cad6a165c81a93e981d42e3a0d9
atulonruby <atuljha86@gmail.com>
2014-04-03 13:25:55 -0700

    Added AWS logic

--- lib/getaway_aws.rb
+++ lib/getaway_aws.rb
@@ -1,11 +1,28 @@
 require 'aws-sdk'
 
 module GetawayAws
-  def car
-    "first time i saw you, you were chasing down...A Cylcone, all alone in a field"
-  end
-  
-  def get
-    AWS::S3.new
+  class Uploader
+     attr_accessor :file, :file_name_in_bucket, :bucket_name
+
+     def initialize(options={})
+
+       @file_name_in_bucket = options[:file_name] 
+       @file = File.open(options[:path])
+       @bucket_name = options[:bucket_name] || "PracticeSession"
+       @key = options[:key]
+       @secret_key = options[:secret]
+
+       
+       AWS.config(
+         :access_key_id => @key, 
+         :secret_access_key => @secret_key
+       )
+       s3 = AWS::S3.new
+       bucket_name = @bucket_name
+       file_name = @file
+    
+       s3.buckets[bucket_name].objects[@file_name_in_bucket].write(:file => @file)
+     end
+
   end
 end
